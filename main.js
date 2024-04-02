dd9d1af7a61d8935577a4963e2394750c3a19c16
ANDgamer <andriychernysh88@gmail.com>
2024-04-02 22:50:15 +0300

    add js

---
+++ main.js
@@ -0,0 +1,21 @@
+const currentDate = document.getElementById('Date');
+const content = document.getElementById('Content');
+
+function trackScroll() {
+    var scrolledPixels = content.scrollTop;
+    if (scrolledPixels >= 8) {
+      if(!currentDate.classList.contains('scrolled')){
+         currentDate.classList.add("scrolled");
+      }}
+    else {
+       if (currentDate.classList.contains('scrolled')) {
+         currentDate.classList.remove('scrolled');
+       }
+    }
+    
+    var topPosition = content.scrollTop;
+    console.log('позиція по Х "' + topPosition + '" px');
+}
+
+// Виклик функції при прокручуванні сторінки
+content.addEventListener('scroll', trackScroll);
