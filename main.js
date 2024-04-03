7576e5df246c3cf0b45027091cc7ab9f81cadc62
ANDgamer <andriychernysh88@gmail.com>
2024-04-03 16:18:46 +0300

    Added timetable

--- main.js
+++ main.js
@@ -2,19 +2,18 @@ const currentDate = document.getElementById('Date');
 const content = document.getElementById('Content');
 
 function trackScroll() {
-    var scrolledPixels = content.scrollTop;
-    if (scrolledPixels >= 8) {
-      if(!currentDate.classList.contains('scrolled')){
-         currentDate.classList.add("scrolled");
-      }}
-    else {
-       if (currentDate.classList.contains('scrolled')) {
-         currentDate.classList.remove('scrolled');
-       }
+  var scrolledPixels = content.scrollTop;
+  if (scrolledPixels >= 8) {
+    if(!currentDate.classList.contains('scrolled')){
+      currentDate.classList.add("scrolled");
     }
-    
-    var topPosition = content.scrollTop;
-    console.log('позиція по Х "' + topPosition + '" px');
+  }
+  else {
+    if (currentDate.classList.contains('scrolled')) {
+      currentDate.classList.remove('scrolled');
+    }
+  }
+  var topPosition = content.scrollTop;
 }
 
 // Виклик функції при прокручуванні сторінки
