e4d0c8afdaf197a58757ba81902e3955735b6caf
ANDgamer <andriychernysh88@gmail.com>
2024-04-10 16:17:13 +0300

    Added auto detecting of day and week

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// Виклик функції при прокручуванні сторінки
 const days = document.querySelectorAll('.day');
 
 function trackScroll(event) {
@@ -14,13 +15,45 @@ function trackScroll(event) {
     }
   }
   var topPosition = container.scrollTop;
-  // console.log(scrolledPixels);
 }
 
-// Виклик функції при прокручуванні сторінки
 days.forEach(element => {
   element.addEventListener('scroll', trackScroll);
 });
+
+// Заміна чисельника або знаменника
+const body = document.body;
+function getCurrentWeekNumber() {
+  var currentDate = new Date();
+  var baseDate = new Date(2024, 1, 12);
+  var diffMilliseconds = currentDate - baseDate;
+  var oneWeekMilliseconds = 7 * 24 * 60 * 60 * 1000;
+  var weekNumber = Math.floor(diffMilliseconds / oneWeekMilliseconds) + 1;
+  return weekNumber;
+}
+
+function isEven(weekNumber) {
+  return weekNumber % 2 === 0;
+}
+
+if (isEven(getCurrentWeekNumber())) {
+  body.classList.remove('num');
+  body.classList.add('den');
+} else {
+  body.classList.remove('den');
+  body.classList.add('num');
+}
+
+//Встановлення поточного дня тижня
+function getCurrentDayOfWeek() {
+  var currentDate = new Date();
+  var dayOfWeek = currentDate.getDay();
+  if (dayOfWeek === 0) {
+    return 6; // Понеділок
+  } else {
+    return dayOfWeek - 1;
+  }
+}
 
 // Swiper js
 const arr = ["ПН", "ВТ", "СР", "ЧТ", "ПТ"]; 
@@ -29,11 +62,12 @@ var swiper = new Swiper(".mySwiper", {
   spaceBetween: 15,
   pagination: {
     el: ".swiper-pagination",
-    bulletClass: "bullet",
+    bulletClass: "dayBtn",
     bulletActiveClass: "active",
     clickable: true,
     renderBullet: function (index, className) { 
       return '<div class="' + className + '">' + arr[index] + "</div>"; 
     }, 
   },
+  initialSlide: getCurrentDayOfWeek()
 });
